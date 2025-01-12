8788b1c1ac0bc88fcd74aa5eddd19b3136f333ab
Marek Hauschwitz <hauschwitz@wpj.cz>
2025-01-12 10:49:38 +0100

    fix nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -597,17 +597,11 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        gopls = {},
-        pyright = {},
+        mypy = {},
         rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
-        --
+        ts_ls = {},
 
         lua_ls = {
           -- cmd = {...},
